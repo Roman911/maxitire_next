01a71991bc2f7c13d0b013a5467d9b1d6b5a58d6
roma <roma-lysyk@ukr.net>
2025-05-20 21:25:35 +0300

    fix

--- components/Product/ImagesBlock/ReactImageGallery.tsx
+++ components/Product/ImagesBlock/ReactImageGallery.tsx
@@ -1,30 +1,33 @@
 import { FC, useRef } from 'react';
-import ImageGallery from "react-image-gallery";
+import ImageGallery, { ReactImageGalleryItem } from 'react-image-gallery';
 import { Photo } from '@/models/product';
 import './index.scss';
 
 interface Props {
-	images: Photo[]
+	images: Photo[];
 	photo: {
-		url_part: string
-		url_part2: string
-	}
+		url_part: string;
+		url_part2: string;
+	};
 }
 
-export const ReactImageGallery: FC<Props> = ({ photo, images = [] }) => {
-	const imageGalleryRef = useRef<any>(null);
+// Extend the ImageGallery type to include the toggleFullScreen method
+type ImageGalleryWithMethods = {
+	toggleFullScreen: () => void;
+};
 
-	const onePhoto = {
+export const ReactImageGallery: FC<Props> = ({ photo, images = [] }) => {
+	const imageGalleryRef = useRef<ImageGalleryWithMethods | null>(null);
+
+	const onePhoto: ReactImageGalleryItem = {
 		original: photo.url_part,
-		thumbnail: photo.url_part2
+		thumbnail: photo.url_part2,
 	};
 
-	const imgArr = images.map(item => {
-		return {
-			original: item.big,
-			thumbnail: item.small
-		}
-	});
+	const imgArr: ReactImageGalleryItem[] = images.map((item) => ({
+		original: item.big,
+		thumbnail: item.small,
+	}));
 
 	const items = [ onePhoto, ...imgArr ];
 
@@ -33,14 +36,14 @@ export const ReactImageGallery: FC<Props> = ({ photo, images = [] }) => {
 	};
 
 	return (
-		<div className='max-w-72 mx-auto'>
+		<div className="max-w-72 mx-auto">
 			<ImageGallery
-				items={items}
+				items={ items }
 				showPlayButton={ false }
-				ref={ imageGalleryRef }
+				ref={ imageGalleryRef as any } // safe because we control the type above
 				onClick={ onClickHandler }
 				showThumbnails={ items.length > 1 }
 			/>
 		</div>
-	)
+	);
 };
